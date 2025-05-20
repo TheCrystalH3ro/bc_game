2899a3fd7d530e60ac79545688634ec8ae0e0631
TheCrystalH3ro <matusnema@gmail.com>
2025-05-20 13:55:14 +0200

    Metóda na odosielanie RPC pre skupinu hráčov

--- Assets/Scripts/Modules/ConnectionModule.cs
+++ Assets/Scripts/Modules/ConnectionModule.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using Assets.Scripts.Responses;
 using FishNet;
 using FishNet.Connection;
@@ -13,12 +14,12 @@ namespace Assets.Scripts.Modules
         private static ConnectionModule _instance;
 
         public static ConnectionModule Singleton
-        { 
+        {
             get
             {
                 if (_instance == null)
                 {
-                    _instance =  new(ConfigModule.Get("API_URL"));
+                    _instance = new(ConfigModule.Get("API_URL"));
                 }
 
                 return _instance;
@@ -38,26 +39,28 @@ namespace Assets.Scripts.Modules
         public void HostServer()
         {
             Debug.Log("Starting the server");
-            InstanceFinder.ServerManager.StartConnection((ushort) serverPort);
+            InstanceFinder.ServerManager.StartConnection((ushort)serverPort);
         }
 
         public void JoinServer()
         {
             Debug.Log("Joining the server");
-            InstanceFinder.ClientManager.StartConnection(serverHost, (ushort) serverPort);	
+            InstanceFinder.ClientManager.StartConnection(serverHost, (ushort)serverPort);
         }
 
         public IEnumerator VerifyPlayer(NetworkConnection client, string playerToken, uint characterId, Action<NetworkConnection, CharacterResponse> onVerificationSuccess, Action<NetworkConnection, UnityWebRequest> onVerificationFail)
         {
             string verifyUrl = apiUrl + "/character/" + characterId;
 
-            using (UnityWebRequest request = UnityWebRequest.Get(verifyUrl)) {
+            using (UnityWebRequest request = UnityWebRequest.Get(verifyUrl))
+            {
 
                 request.SetRequestHeader("Authorization", playerToken);
 
                 yield return request.SendWebRequest();
 
-                if (request.result != UnityWebRequest.Result.Success) {
+                if (request.result != UnityWebRequest.Result.Success)
+                {
                     onVerificationFail?.Invoke(client, request);
                     yield break;
                 }
@@ -65,6 +68,20 @@ namespace Assets.Scripts.Modules
                 CharacterResponse characterResponse = CharacterResponse.CreateFromJSON(request.downloadHandler.text);
 
                 onVerificationSuccess?.Invoke(client, characterResponse);
+            }
+        }
+
+        public void GroupRpc(List<NetworkConnection> connections, Delegate rpcMethod, params object[] args)
+        {
+            object[] rpcParams = new object[args.Length + 1];
+            
+            rpcParams[0] = null;
+            Array.Copy(args, 0, rpcParams, 1, args.Length);
+            
+            foreach (NetworkConnection connection in connections)
+            {
+                rpcParams[0] = connection;
+                rpcMethod.DynamicInvoke(rpcParams);
             }
         }
     }
